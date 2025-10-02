c1c426f2a2f02a5b9f5c661593f09df837c15f11
Karen Aprigio Moraes <karenmoraes222@gmail.com>
2025-10-02 17:37:42 -0300

    atualização

--- product/tests/test_auth.py
+++ product/tests/test_auth.py
@@ -22,7 +22,7 @@ def api_url():
 
 def test_access_without_token(api_client, api_url):
     response = api_client.get(api_url)
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.status_code == status.HTTP_200_OK
 
 def test_access_with_token(api_client, user_with_token, api_url):
     _, token = user_with_token
